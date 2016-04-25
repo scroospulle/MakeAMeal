ff0c1b9eb44a342d6a13977d778bc5e66dc6755f
d2118370 <d2118370@gmail.com>
2016-04-25 19:17:54 -0400

    api

--- Platform_Api/indexA.php
+++ Platform_Api/indexA.php
@@ -3,11 +3,19 @@
 </head>
 <body>
 	<a>
-		<h1>Get Skills:</h1>
-		<form action="getRecipe.php" method="post">
+		<h1>Get Recipe:</h1>
+		<form action="getSkills.php" method="post">
 			ingred: <br><input type="text" name="ingredient"><br>
 			<input type="submit">
 		</form>
 	</a>
+	<a>
+		<h1>Get Recipe by id:</h1>
+		<form action="getRecipeById.php" method="post">
+			r_id: <br><input type="number" name="rid"><br>	
+			<input type="submit">
+		</form>
+	</a>
+	
 </body>
 </html>
